94c1a4dc46883f25e6d7c95bb0d1ca9a2f9795ee
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-23 08:21:55 +0200

    BONUS 1 AND BONUS 2

---
+++ BONUS/js/main.js
@@ -0,0 +1,68 @@
+const {createApp} = Vue;
+
+const app = createApp({
+    data(){
+        return {
+            toDoList: [
+                {
+                    text: "titolo 1",
+                    done: false,
+                },
+                {
+                    text: "titolo 2",
+                    done: false,
+                },
+                {
+                    text: "titolo 3",
+                    done: true,
+                },
+                {
+                    text: "titolo 4",
+                    done: false,
+                },
+                {
+                    text: "titolo 5",
+                    done: false,
+                },
+                {
+                    text: "titolo 6",
+                    done: true,
+                },
+                {
+                    text: "titolo 7",
+                    done: false,
+                },
+                {
+                    text: "titolo 8",
+                    done: false,
+                }
+            ],
+        }
+    },
+    methods:{
+        deleteToDo(id){
+            console.log(this.toDoList[id])
+            this.toDoList.splice(id, 1)
+        },
+        addToDo(event){
+            console.log("ciao")
+            inputValue = event.target.parentElement.children[0].value;
+            if(inputValue.length < 5){
+                alert("Inserisci una parola di almeno 5 lettere");
+            }
+            else if(inputValue.length > 80){
+                alert("Inserisci al massimo 80 caratteri");
+            }
+            else {
+                this.toDoList.push({
+                    text: inputValue,
+                    done: false
+                })
+                event.target.parentElement.children[0].value = ""
+            }
+        },
+        invertDone(id){
+            this.toDoList[id].done = !this.toDoList[id].done;
+        }
+    }
+}).mount("#app")
