bc84498998dc77eacf3af8ba0e254d20e2e37797
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-23 07:01:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const {createApp} = Vue;
+
+const app = createApp({
+    data(){
+        return {
+            toDoList: [
+                {
+                    text: "titolo 1",
+                    done: false,
+                },
+                {
+                    text: "titolo 2",
+                    done: false,
+                },
+                {
+                    text: "titolo 3",
+                    done: true,
+                },
+                {
+                    text: "titolo 4",
+                    done: false,
+                },
+                {
+                    text: "titolo 5",
+                    done: false,
+                },
+                {
+                    text: "titolo 6",
+                    done: true,
+                },
+                {
+                    text: "titolo 7",
+                    done: false,
+                },
+                {
+                    text: "titolo 8",
+                    done: false,
+                }
+            ],
+        }
+    }
+}).mount("#app")
